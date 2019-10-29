606a5698c899984d0a874edb84d4e55b629a2bcb
Tiltustratel <tiltustratel@gmail.com>
2019-10-29 17:00:08 +1300

    Base prototype created.

---
+++ Skipper2AssetReplacer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Skipper2AssetReplacer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Open = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btnLeft = new System.Windows.Forms.Button();
+			this.btnRight = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.numIndex = new System.Windows.Forms.NumericUpDown();
+			this.Goto = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numIndex)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Open
+			// 
+			this.Open.Location = new System.Drawing.Point(12, 12);
+			this.Open.Name = "Open";
+			this.Open.Size = new System.Drawing.Size(91, 23);
+			this.Open.TabIndex = 0;
+			this.Open.Text = "Open mm2.dat";
+			this.Open.UseVisualStyleBackColor = true;
+			this.Open.Click += new System.EventHandler(this.Open_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(640, 420);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// btnLeft
+			// 
+			this.btnLeft.Location = new System.Drawing.Point(109, 12);
+			this.btnLeft.Name = "btnLeft";
+			this.btnLeft.Size = new System.Drawing.Size(22, 23);
+			this.btnLeft.TabIndex = 2;
+			this.btnLeft.Text = "<";
+			this.btnLeft.UseVisualStyleBackColor = true;
+			this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+			// 
+			// btnRight
+			// 
+			this.btnRight.Location = new System.Drawing.Point(137, 12);
+			this.btnRight.Name = "btnRight";
+			this.btnRight.Size = new System.Drawing.Size(22, 23);
+			this.btnRight.TabIndex = 3;
+			this.btnRight.Text = ">";
+			this.btnRight.UseVisualStyleBackColor = true;
+			this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(165, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(91, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Replace";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numIndex
+			// 
+			this.numIndex.Location = new System.Drawing.Point(532, 12);
+			this.numIndex.Maximum = new decimal(new int[] {
+            638,
+            0,
+            0,
+            0});
+			this.numIndex.Name = "numIndex";
+			this.numIndex.Size = new System.Drawing.Size(120, 20);
+			this.numIndex.TabIndex = 5;
+			// 
+			// Goto
+			// 
+			this.Goto.Location = new System.Drawing.Point(451, 10);
+			this.Goto.Name = "Goto";
+			this.Goto.Size = new System.Drawing.Size(75, 23);
+			this.Goto.TabIndex = 6;
+			this.Goto.Text = "Goto";
+			this.Goto.UseVisualStyleBackColor = true;
+			this.Goto.Click += new System.EventHandler(this.Goto_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(664, 472);
+			this.Controls.Add(this.Goto);
+			this.Controls.Add(this.numIndex);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.btnRight);
+			this.Controls.Add(this.btnLeft);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.Open);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numIndex)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Open;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnLeft;
+		private System.Windows.Forms.Button btnRight;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numIndex;
+		private System.Windows.Forms.Button Goto;
+	}
+}
+
---
+++ Skipper2AssetReplacer/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Skipper2AssetReplacer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private string FilePath = string.Empty;
+		private byte[] FileData;
+
+		/// <summary>
+		/// Allows the user to choose a Skipper and Skeeto 2 save file to load the values from.
+		/// Reads each 4 byte chunk of data into the FileData array.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Open_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
+			{
+				//openFileDialog.InitialDirectory = "C:\\Users\\dndga\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Skipper2";
+				openFileDialog.InitialDirectory = "C:\\Program Files (x86)\\Skipper2\\";
+				openFileDialog.Filter = "dat files (*.dat)|*.dat";
+				openFileDialog.FilterIndex = 2;
+				openFileDialog.RestoreDirectory = true;
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					FilePath = openFileDialog.FileName;
+					FileData = File.ReadAllBytes(FilePath);
+					GetImage(0);
+				}
+			} 
+		}
+
+		private void GetImage (int index)
+		{
+			imageByteIndex = GetPositionAfterMatch(FileData, bmpPattern, index);
+
+			//Console.WriteLine(offset);
+
+			FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+			byte[] bmpBytes = new byte[imageWidth * imageHeight + 5000];
+			Console.WriteLine(imageWidth * imageHeight);
+			fs.Position = imageByteIndex;
+			fs.Read(bmpBytes, 0, bmpBytes.Length);
+			fs.Close();
+			using (var ms = new MemoryStream(bmpBytes))
+			{
+				pictureBox1.Image = new Bitmap(ms);
+			}
+		}
+
+		public static Bitmap BytesToImage(byte[] bytes)
+		{
+			MemoryStream mStream = new MemoryStream();
+			byte[] pData = bytes;
+			mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+			Bitmap bm = new Bitmap(mStream, false);
+			mStream.Dispose();
+			return bm;
+		}
+
+		byte[] bmpPattern = new byte[] { 0x42, 0x4D, 0x36 };
+
+		int GetPositionAfterMatch(byte[] data, byte[] pattern, int index)
+		{
+			Console.WriteLine(index);
+			int matchIndex = 0;
+			for (int i = 0; i < data.Length - 3; i++)
+			{
+				bool match = true;
+				for (int k = 0; k < 3; k++)
+				{
+					// As soon as the pattern is broken, reset
+					if (data[i + k] != pattern[k])
+					{
+						match = false;
+					}
+				}
+				if (match)
+				{
+					if(matchIndex == index)
+					{
+						// Find width and height and set bytearray to (width * height + 1078)
+						using (FileStream fs = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+						{
+							byte[] widthBytes = new byte[2];
+							byte[] heightBytes = new byte[2];
+							fs.Position = i + 18;
+							fs.Read(widthBytes, 0, 2);
+							fs.Position = i + 22;
+							fs.Read(heightBytes, 0, 2);
+							imageWidth = BitConverter.ToInt16(widthBytes, 0);
+							imageHeight = BitConverter.ToInt16(heightBytes, 0);
+							//Console.WriteLine("Width: " + BitConverter.ToInt16(widthBytes, 0));
+							//Console.WriteLine("Height: " + BitConverter.ToInt16(heightBytes, 0));
+						}
+						return i;
+					}
+					matchIndex++;
+				}
+			}
+			return 69;
+		}
+
+		int imageWidth = 0;
+		int imageHeight = 0;
+		int imageByteIndex = 0;
+
+		/// <summary>
+		/// Deals with endianness by flipping the order of bytes
+		/// </summary>
+		private int ReverseBytes(UInt32 value)
+		{
+			return (int)((value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 | (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24);
+		}
+
+		int imageIndex = 0;
+
+		private void btnLeft_Click(object sender, EventArgs e)
+		{
+			if (imageIndex == 0) return;
+			imageIndex--;
+			GetImage(imageIndex);
+		}
+
+		private void btnRight_Click(object sender, EventArgs e)
+		{
+			imageIndex++;
+			GetImage(imageIndex);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
+			{
+				openFileDialog.InitialDirectory = "C:\\";
+				openFileDialog.Filter = "bmp files (*.bmp)|*.bmp";
+				openFileDialog.FilterIndex = 2;
+				openFileDialog.RestoreDirectory = true;
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					byte[] ImageData = File.ReadAllBytes(openFileDialog.FileName);
+					int targetByteSize = imageWidth * imageHeight + 1078;
+					Console.WriteLine(ImageData.Length);
+					// TODO: CHECK THAT COLOR COUNT MATCHES (SHOULD ALWAYS BE 256 COLOR BMP)
+					if(ImageData.Length != targetByteSize)
+					{
+						DialogResult dialogResult = MessageBox.Show(string.Format("Image is not same size, are you sure you want to do this?\n{0}/{1} ({2} byte difference)", ImageData.Length, targetByteSize, targetByteSize - ImageData.Length), "File Size Mismatch", MessageBoxButtons.YesNo);
+						if (dialogResult == DialogResult.Yes)
+						{
+							ReplaceBytes(ImageData);
+						}
+					} else
+					{
+						ReplaceBytes(ImageData);
+					}
+					
+				}
+			}
+		}
+
+		private void ReplaceBytes (byte[] ImageData)
+		{
+			string newPath = FilePath.Replace(".dat", "_2.dat");
+			File.WriteAllBytes(newPath, FileData);
+			// Find width and height and set bytearray to (width * height + 1078)
+			using (FileStream fs = new FileStream(newPath, FileMode.Open, FileAccess.Write))
+			{
+				fs.Position = imageByteIndex;
+				fs.Write(ImageData, 0, ImageData.Length);
+				MessageBox.Show("Done");
+			}
+		}
+
+		private void Goto_Click(object sender, EventArgs e)
+		{
+			imageIndex = (int)numIndex.Value;
+			GetImage(imageIndex);
+		}
+	}
+}
